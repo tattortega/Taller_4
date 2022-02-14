f9279f5c9bf86f53c515a889da6006f03b0771bc
Ricardo Ortega <tattortega.28@gmail.com>
2022-02-14 11:48:21 -0500

    Ending exercise 1

--- Exercise_1/src/PlanetarySystem/Main.java
+++ Exercise_1/src/PlanetarySystem/Main.java
@@ -35,7 +35,7 @@ public class Main {
 
         System.out.println("""
                 Bienvenido al Sistema Planetario \n              
-                Aqui podrás calcular la atraccion gravitatoria entre dos cuerpos celestes
+                Aqui podrás calcular la atraccion gravitatoria entre dos planetas del sistema solar.
                 Eliga los dos planetas que deseas calcular:
                 1.Mercurio
                 2.Venus
